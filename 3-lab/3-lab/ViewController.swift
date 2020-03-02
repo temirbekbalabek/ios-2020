1b8dfe3c1fccc86762bc4eb3e780ecb4d5018958
Temirbek <temirbek.balabek@mail.ru>
2020-03-02 15:37:06 +0600

    navigation controller

---
+++ 3-lab/3-lab/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  3-lab
+//
+//  Created by Temirbek Balabek on 3/2/20.
+//  Copyright © 2020 kbtu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
